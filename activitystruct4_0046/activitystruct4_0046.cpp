c4e3807c3911d8aa7ab37c6364b3e155e2fc6412
anandajuliar <anandajulia001@gmail.com>
2024-04-02 20:11:46 +0700

    fux problem

--- activitystruct4_0046/activitystruct4_0046.cpp
+++ activitystruct4_0046/activitystruct4_0046.cpp
@@ -7,42 +7,45 @@ struct DetailAlamat {
 	string kota;
 };
 
-struct mahasiswa {
+struct Mahasiswa {
 	string nim;
 	string nama;
 	DetailAlamat alamat;
-	int umur;
+	string umur;
 };
 
-int main() {
-	mahasiswa mhs[3];
+int main()
+{
+	Mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++) {
 		cout << "data ke-" << (i + 1) << ":" << endl;
-		cout << "Nomor mahasiswa : ";
+		cout << "Nomor mahasiswa: ";
 		getline(cin, mhs[i].nim);
-		cout << "Nama Mahasiswa : ";
-		getline(cin, mhs[i].nama);
+		cout << "Nama Mahasiswa: ";
+		getline(cin, mhs[i].nama); 
 
-		cout << "alamat mahasiswa : " << endl;
-		cout << "\t nama Desa : ";
+		cout << "Alamat Mahasiswa: " << endl;
+		cout << "\t Nama Desa: ";
 		cin >> mhs[i].alamat.desa;
-		cout << "\t Nama kota : ";
+		cout << "\t Nama Kota: ";
 		cin >> mhs[i].alamat.kota;
-		cout << "Umur mahasiswa : ";
+
+		cout << "Umur mahasiswa: ";
 		cin >> mhs[i].umur;
 		cin.ignore(1, '\n');
 		cout << endl;
 	}
 
 	for (int i = 0; i < 3; i++) {
+
 		cout << endl;
-		cout << "Data mahasiswa ke-" << (i = 1) << ":" << endl;
-		cout << "\n Nim :" << mhs[i].nim;
-		cout << "\n Nama : " << mhs[i].nama;
-		cout << "\n alamat : ";
-		cout << "\n \t Desa : " << mhs[i].alamat.desa;
-		cout << "\n \t Kota : " << mhs[i].alamat.kota;
-		cout << "\n Umur : " << mhs[i].umur;
+		cout << "Data mahasiswa ke-" << (i + 1) << ":" << endl;
+		cout << "\n NIM: " << mhs[i].nim;
+		cout << "\n Nama: " << mhs[i].nama;
+		cout << "\n Alamat: ";
+		cout << "\n \tDesa: " << mhs[i].alamat.desa;
+		cout << "\n \tKota: " << mhs[i].alamat.kota;
+		cout << "\n Umur: " << mhs[i].umur;
 		cout << endl;
 	}
 }
