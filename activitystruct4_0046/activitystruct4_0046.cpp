d592cbf5786fa1eea3b8743bb62b95d625bc5084
anandajuliar <anandajulia001@gmail.com>
2024-04-02 18:50:04 +0700

    for 1 vol 2

--- activitystruct4_0046/activitystruct4_0046.cpp
+++ activitystruct4_0046/activitystruct4_0046.cpp
@@ -21,3 +21,14 @@ int main() {
 		getline(cin, mhs[i].nim);
 		cout << "Nama Mahasiswa : ";
 		getline(cin, mhs[i].nama);
+
+		cout << "alamat mahasiswa : " << endl;
+		cout << "\t nama Desa : ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t Nama kota : ";
+		cin >> mhs[i].alamat.kota;
+		cout << "Umur mahasiswa : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
+	}
